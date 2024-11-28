9a9cf414201bab2aa9f3c2b2903a20fbefda491d
ArtHavoc <90911022+ArtHavoc@users.noreply.github.com>
2024-11-28 17:16:00 -0300

    Create site-info.go

---
+++ site-info.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"regexp"
+)
+
+// <-chan - canal somente leitura
+
+func GetSiteName(urls ...string) <-chan string {
+	c := make(chan string)
+
+	for _, url := range urls {
+		//fmt.Println(url)
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := io.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*)<\\/title>")
+			c <- r.FindStringSubmatch(string(html))[1]
+		}(url)
+	}
+
+	return c
+}
+
+func PrintChannel(ch <-chan string) {
+	for c := range ch {
+		fmt.Println(c)
+	}
+}
